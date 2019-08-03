5b7d33c45cd745876892776fdb5316060fc1b5bc
eanastasio <eanastasio@veracruz.gob.mx>
2019-08-03 09:57:54 -0500

    app redux

---
+++ src/app/todo/model/todo.model.ts
@@ -0,0 +1,13 @@
+export class Todo {
+    public id: number;
+    public texto: string;
+    public completado: boolean;
+
+    constructor(text: string ){
+        this.texto = text.charAt(0).toUpperCase() + text.slice(1);
+        // this.texto = text;
+        this.completado = false;
+
+        this.id = Math.random();
+    }
+}
